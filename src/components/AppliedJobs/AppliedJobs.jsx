6993f4609af1ef10f5a2fcc46a7e87e8c4d6fb0d
robiulXhasan <robi16275@gmail.com>
2023-04-11 23:34:14 +0600

    filtered option done

--- src/components/AppliedJobs/AppliedJobs.jsx
+++ src/components/AppliedJobs/AppliedJobs.jsx
@@ -6,8 +6,14 @@ import SharedBanner from "../SharedBanner/SharedBanner";
 const AppliedJobs = () => {
   const jobs = useLoaderData();
   const [appliedJobs, setAppliedJobs] = useState(jobs);
-  
-
+  const handleFilter = () => {
+    var filterOption = document.getElementById("mySelect").value;
+    if (filterOption) {
+      let filteredJobs = jobs.filter((job) => job.remote_or_onsite === filterOption);
+      console.log(filteredJobs);
+      setAppliedJobs(filteredJobs);
+    }
+  };
 
   return (
     <div>
@@ -15,7 +21,7 @@ const AppliedJobs = () => {
       <div className="w-10/12 mx-auto mt-24">
         <div className="text-end">
           <select
-          
+            onChange={handleFilter}
             className="border border-1 border-slate-500 p-2 rounded-md"
             id="mySelect"
           >
@@ -24,9 +30,11 @@ const AppliedJobs = () => {
             <option value="Onsite">Onsite</option>
           </select>
         </div>
-        {appliedJobs.map((job) => (
-          <AppliedJob key={job.id} job={job}></AppliedJob>
-        ))}
+        <div className="mb-16">
+          {appliedJobs.map((job) => (
+            <AppliedJob key={appliedJobs.id} job={job}></AppliedJob>
+          ))}
+        </div>
       </div>
     </div>
   );
