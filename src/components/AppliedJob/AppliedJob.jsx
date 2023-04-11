5f5083d98fda657b1fcc73dd229a5a623a722aa6
robiulXhasan <robi16275@gmail.com>
2023-04-12 01:09:23 +0600

    applied jobs page make responsive

--- src/components/AppliedJob/AppliedJob.jsx
+++ src/components/AppliedJob/AppliedJob.jsx
@@ -15,9 +15,9 @@ const AppliedJob = ({ job }) => {
   } = job;
   return (
     <div>
-      <div className="flex mt-5 items-center border rounded-md p-3 justify-between">
-        <div className="flex gap-5">
-          <img className="h-44 w-72 rounded-md bg-indigo-50 p-4" src={company_logo} alt="" />
+      <div className="md:flex mt-5 items-center border rounded-md p-3 justify-between">
+        <div className="md:flex gap-5">
+          <img className="h-44 w-72 bg-indigo-50 p-4" src={company_logo} alt="" />
           <div className="space-y-2 p-5">
             <h4 className="text-3xl font-medium">{job_title}</h4>
             <p className="text-[#524c4c]">{company_name}</p>
@@ -41,7 +41,7 @@ const AppliedJob = ({ job }) => {
           </div>
         </div>
         <Link to={`/${id}`}>
-          <button className="px-4 py-2 mt-5 text-md bg-gradient-to-r from-indigo-500 to-indigo-400 font-medium rounded-md text-white mb-10 md:mb-0 me-5">
+          <button className="px-4 py-2 mt-5 text-md bg-gradient-to-r from-indigo-500 to-indigo-400 font-medium rounded-md text-white mb-10 md:mb-0 ">
             View Details
           </button>
         </Link>
--- src/components/SharedBanner/SharedBanner.jsx
+++ src/components/SharedBanner/SharedBanner.jsx
@@ -4,9 +4,9 @@ import bannerImg2 from "../../../src/assets/All Images/Vector-1.png";
 
 const SharedBanner = ({ bannerHeading }) => {
   return (
-    <div className="bg-indigo-50 h-[200px] md:h-[400px] md:flex justify-between ">
-      <img className="h-48 mt-52 hidden md:block" src={bannerImg1} alt="" />
-      <h2 className="pt-10 md:mt-32 text-4xl text-center font-bold">{bannerHeading}</h2>
+    <div className="bg-indigo-50 h-[200px] md:h-[290px] md:flex justify-between ">
+      <img className="h-48 mt-24 hidden md:block" src={bannerImg1} alt="" />
+      <h2 className="pt-10 md:mt-12 text-4xl text-center font-bold">{bannerHeading}</h2>
       <img className="h-48 mt-[-110px]  hidden md:block" src={bannerImg2} alt="" />
     </div>
   );
